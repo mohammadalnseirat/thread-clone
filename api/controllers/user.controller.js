f2c13a02054e6b0be93cf354bd41524b94671a20
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-05 21:41:50 +0400

    Create User Model

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,6 @@
+import User from "../models/user.model.js";
+import { handleErrors } from "../utils/error.js";
+
 // 1-Function to test the route:
 export const test_get = (req, res, next) => {
   res.send("This is a test route");
---
+++ api/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: [String],
+      default: [],
+    },
+    following: {
+      type: [String],
+      default: [],
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    isForizon: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// Create a model from the schema:
+const User = mongoose.model("User", userSchema);
+
+export default User;
