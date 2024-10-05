6b44de68b6c67b9b9ddc2cb3a606b309e0413586
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-05 21:13:18 +0400

    Create MiddleWare And Function To Handle Errors

--- api/index.js
+++ api/index.js
@@ -15,3 +15,14 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectMongoDataBase();
 });
+
+// Middleware To Handle Errors:
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success:false,
+    message,
+    statusCode
+  })
+})
