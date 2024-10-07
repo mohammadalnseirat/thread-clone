36df76b822bc6b2d6a91c694948c851d13088324
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-07 23:53:44 +0400

    Create Sign Up Card Component UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import {  Container } from '@chakra-ui/react'
-import { Route, Routes } from 'react-router-dom'
-import React from 'react'
-import UserPage from './pages/UserPage'
-import PostPage from './pages/PostPage'
-import Header from './Components/Header'
-import HomePage from './pages/HomePage'
+import { Container } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+import UserPage from "./pages/UserPage";
+import PostPage from "./pages/PostPage";
+import Header from "./Components/Header";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
 
 const App = () => {
   return (
-    <Container maxW='620px'>
-      <Header/>
+    <Container maxW="620px">
+      <Header />
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/:username' element={<UserPage/>}/>
-        <Route path='/:username/post/:pId' element={<PostPage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/:username" element={<UserPage />} />
+        <Route path="/:username/post/:pId" element={<PostPage />} />
       </Routes>
     </Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/AuthPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignupCard from '../Components/SignupCard'
+
+const AuthPage = () => {
+  return (
+    <>
+    <SignupCard/>
+    </>
+  )
+}
+
+export default AuthPage
