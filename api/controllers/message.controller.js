ec3ccc4059a0692c86d648143c82d48611270057
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-17 22:39:47 +0400

    Create Get All messages Api Route

--- api/controllers/message.controller.js
+++ api/controllers/message.controller.js
@@ -54,4 +54,31 @@ const sendMessage_Post = async (req, res, next) => {
   }
 };
 
-export { sendMessage_Post };
+// !2-Function To Get Messages:
+const getAllMessages_Get = async (req, res, next) => {
+  const { otheruserId } = req.params;
+  const userId = req.user._id;
+  try {
+    // !Find The Conversation:
+    const conversation = await Conversation.findOne({
+      participants: {
+        $all: [userId, otheruserId],
+      },
+    });
+    if (!conversation) {
+      return next(handleErrors(404, "Conversation not found!"));
+    }
+    // !Find All Messages:
+    const messages = await Message.find({
+      conversationId: conversation._id,
+    }).sort({ createdAt: 1 });
+
+    // !Send response back:
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getting messages:", error.message);
+    next(error);
+  }
+};
+
+export { sendMessage_Post, getAllMessages_Get };
--- api/routes/message.route.js
+++ api/routes/message.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { sendMessage_Post } from "../controllers/message.controller.js";
+import {
+  getAllMessages_Get,
+  sendMessage_Post,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.post("/", protectRoute, sendMessage_Post);
+router.get("/:otheruserId", protectRoute, getAllMessages_Get);
 
 export default router;
